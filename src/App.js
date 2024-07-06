94a66c5d6bcdd83a5209c2150a652c6a057e0c9e
jayalakshmi610 <jayalakshmi61015@gmail.com>
2024-07-06 21:13:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { GET_USERS } from "./queries";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post("http://localhost:4000/graphql", {
+          query: GET_USERS,
+        });
+        setData(response.data.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users</h1>
+      <ul>
+        {data.users.map(({ id, name, email }) => (
+          <li key={id}>
+            <p>Name: {name}</p>
+            <p>Email: {email}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
---
+++ src/queries.js
@@ -0,0 +1,11 @@
+export const GET_USERS = `
+  query {
+    users(id: "1") {
+      id
+      name
+      email
+      linkedin
+      id
+    }
+  }
+`;
